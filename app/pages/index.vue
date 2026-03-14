5f8bde42d5cb367a9f4de6fe2738b70f9f9e1249
hyna42 <bylo@duck.com>
2026-03-14 18:53:10 +0100

    feat : handle when API error

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -49,6 +49,7 @@ console.log("data", data.value);
       <h2 class="text-3xl lg:text-5xl mb-2">Discover, Create, Share</h2>
       <p class="text-lg lg:text-xl mb-8">Check out our most popular recipes!</p>
       <div
+        v-if="!error"
         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8"
       >
         <div
@@ -95,6 +96,9 @@ console.log("data", data.value);
           </div>
         </div>
       </div>
+      <p v-else class="text-xl">
+        Opps, something went wrong. Please try again later
+      </p>
     </section>
   </main>
 </template>
