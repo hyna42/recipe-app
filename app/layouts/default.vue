a595cb78003019f5b98f09b6dd7495d74b2e4c03
hyna42 <bylo@duck.com>
2026-03-14 02:13:29 +0100

    setup : structure, layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"></script>
+<template>
+  <div>
+    <p>Default Layout</p>
+    <slot></slot>
+  </div>
+</template>
---
+++ app/pages/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+definePageMeta({
+  layout: "login",
+});
+</script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+  </div>
+</template>
